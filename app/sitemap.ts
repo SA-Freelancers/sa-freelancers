26ea6e540e373eedbac0aa3305e531417c47ccf3
SA-Freelancers <thabangkhosi1996@gmail.com>
2026-06-16 12:01:05 +0200

    Add robots and sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,19 +1,39 @@
-export default function sitemap() {
+import { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  const baseUrl = "https://freelancehubsa.co.za";
+
   return [
     {
-      url: "https://freelancehubsa.co.za",
+      url: baseUrl,
       lastModified: new Date(),
     },
     {
-      url: "https://freelancehubsa.co.za/search",
+      url: `${baseUrl}/search`,
       lastModified: new Date(),
     },
     {
-      url: "https://freelancehubsa.co.za/faq",
+      url: `${baseUrl}/about`,
       lastModified: new Date(),
     },
     {
-      url: "https://freelancehubsa.co.za/support",
+      url: `${baseUrl}/support`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/faq`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/safety`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/terms`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/privacy`,
       lastModified: new Date(),
     },
   ];
